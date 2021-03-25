6734cc78d05998b5b21df15a14d35e5304c8d92c
DanielVillasmil <dvillasmil08@gmail.com>
2021-03-24 21:13:08 -0400

    corrected branding.js files

--- src/views/forms/Branding.js
+++ src/views/forms/Branding.js
@@ -58,65 +58,37 @@ function Branding() {
                 
                     <div>
                     {/* <p>Streamer Package<a href="/branding-dir/wildrp_streamer.zip">Download File</a></p> */}
-                    <p>Logo White<a style={{color:'#e4b85d'}} href="../../assets/branding-dir/logo.zip">Download File</a></p>
-                    <p>Logo Print White<a style={{color:'#e4b85d'}} href="/branding-dir/logo-print.zip">Download File</a></p>
-                    <p>Icon 24 x 24 (W)<a style={{color:'#e4b85d'}} href="/branding-dir/24x24.zip">Download File</a></p>
-                    <p>Icon 48 x 48 (W)<a style={{color:'#e4b85d'}} href="/branding-dir/48x48.zip">Download File</a></p>
-                    
-                    
-                    <p>Icon 96 x 96 (W)<a style={{color:'#e4b85d'}} href="../../assets/img/branding-dir/96x96-W.png" >Download File</a></p>
-                    
-                    
-                    <p>Icon 144 x 144 (W)<a style={{color:'#e4b85d'}} href="/branding-dir/144x144.zip">Download File</a></p>
-                    <p>Icon 192 x 192 (W)<a style={{color:'#e4b85d'}} href="/branding-dir/192x192.zip">Download File</a></p>
-                    <p>Icon 512 x 512 (W)<a style={{color:'#e4b85d'}}href="/branding-dir/512x512.zip">Download File</a></p>
-                    <p>Icon 1000 x 1000 (W)<a style={{color:'#e4b85d'}} href="/branding-dir/1000x1000.zip">Download File</a></p>
-
-                    <p>Logo Black<a style={{color:'#e4b85d'}} href="/branding-dir/logo.zip">Download File</a></p>
-                    <p>Logo Print Black<a style={{color:'#e4b85d'}} href="/branding-dir/logo-print.zip">Download File</a></p>
-                    <p>Icon 24 x 24 (B)<a style={{color:'#e4b85d'}} href="/branding-dir/24x24.zip">Download File</a></p>
-                    <p>Icon 48 x 48 (B)<a style={{color:'#e4b85d'}} href="/branding-dir/48x48.zip">Download File</a></p>
-                    <p>Icon 96 x 96 (B)<a style={{color:'#e4b85d'}} href="/branding-dir/96x96.zip">Download File</a></p>
-                    <p>Icon 144 x 144 (B)<a style={{color:'#e4b85d'}} href="/branding-dir/144x144.zip">Download File</a></p>
-                    <p>Icon 192 x 192 (B)<a style={{color:'#e4b85d'}} href="/branding-dir/192x192.zip">Download File</a></p>
-                    <p>Icon 512 x 512 (B)<a style={{color:'#e4b85d'}} href="/branding-dir/512x512.zip">Download File</a></p>
-                    <p>Icon 1000 x 1000 (B)<a style={{color:'#e4b85d'}} href="/branding-dir/1000x1000.zip">Download File</a></p>
-                    
-                    
                     <Table borderless className= 'justify-content-center'>
                   <tbody>
                     <tr>
-                      <th><a href='../../assets/branding-dir/logo.zip' style={{textDecoration:'none', color:'#e4b85d'}}>Logo White</a></th>
-                      <th><a href='' style={{textDecoration:'none', color:'#e4b85d'}}>192 x 192 (w)</a></th>
-                      <th><a href='' style={{textDecoration:'none', color:'#e4b85d'}}>48 x 48 (B)</a></th>
-                    
-                    
-                    
+                      <th><a href='' style={{textDecoration:'none', color:'#e4b85d'}}>Logo White</a></th>
+                      <th><a href='' style={{textDecoration:'none', color:'#e4b85d'}}>512 x 512 (w)</a></th>
+                      <th><a href=''style={{textDecoration:'none', color:'#e4b85d'}} >48 x 48 (B)</a></th>
                     </tr>
                     <tr>
-                      <th><a href='https://py.pl/aSFZb' style={{textDecoration:'none', color:'#e4b85d'}}>24 x 24 (W)</a></th>
-                      <th><a href='https://py.pl/1LTaol' style={{textDecoration:'none', color:'#e4b85d'}}>1000 x 1000 (W)</a></th>
+                      <th> <a href='' style={{textDecoration:'none', color:'#e4b85d'}}>Logo Print White</a></th>
+                      <th><a href='' style={{textDecoration:'none', color:'#e4b85d'}}>144 x 144 (W)</a></th>
+                      <th><a href='' style={{textDecoration:'none', color:'#e4b85d'}}>96 x 96 (B)</a></th>
+                    </tr>
+                    <tr>
+                      <th><a href='' style={{textDecoration:'none', color:'#e4b85d'}}>24 x 24 (W)</a></th>
+                      <th><a href='' style={{textDecoration:'none', color:'#e4b85d'}}>1000 x 1000 (W)</a></th>
                       <th><a href='' style={{textDecoration:'none', color:'#e4b85d'}}> 144 x 144 (B) </a></th>
                     </tr>
                     <tr>
-                      <th><a href='https://py.pl/1ciVfH' style={{textDecoration:'none', color:'#e4b85d'}}>48 x 48 (W)</a></th>
-                      <th><a href='https://py.pl/qgMTl' style={{textDecoration:'none', color:'#e4b85d'}}>Logo black</a></th>
+                      <th><a href='' style={{textDecoration:'none', color:'#e4b85d'}}>48 x 48 (W)</a></th>
+                      <th><a href='' style={{textDecoration:'none', color:'#e4b85d'}}>Logo black</a></th>
                       <th> <a href='' style={{textDecoration:'none', color:'#e4b85d'}}>192 x 192 B</a></th>
                     </tr>
                     <tr>
-                      <th><a href='https://py.pl/5ysu9' style={{textDecoration:'none', color:'#e4b85d'}}>96 x 96 (W)</a></th>
-                      <th>Logo<a href='https://py.pl/eehFz'style={{textDecoration:'none', color:'#e4b85d'}} >Logo Print Black</a></th>
-                      <th>Logo <a href='' style={{textDecoration:'none', color:'#e4b85d'}}> 512 x 512 (B)</a></th>
+                      <th><a href='' style={{textDecoration:'none', color:'#e4b85d'}}>96 x 96 (W)</a></th>
+                      <th><a href=''style={{textDecoration:'none', color:'#e4b85d'}} >Logo Print Black</a></th>
+                      <th> <a href='' style={{textDecoration:'none', color:'#e4b85d'}}> 512 x 512 (B)</a></th>
                     </tr>
                     <tr>
-                      <th>Logo<a href='https://py.pl/QNNA3zOV4m' style={{textDecoration:'none', color:'#e4b85d'}}>144 x 144 (W)</a></th>
-                      <th>Logo<a href='https://py.pl/fIqHn'style={{textDecoration:'none', color:'#e4b85d'}} >24 x 24 (B)</a></th>
-                      <th>Logo <a href='' style={{textDecoration:'none', color:'#e4b85d'}}>1000 x 1000 (B)</a></th>
-                    </tr>
-                    <tr>
-                      <th>Logo<a href='https://py.pl/QNNA3zOV4m' style={{textDecoration:'none', color:'#e4b85d'}}>192 x 192 (W)</a></th>
-                      <th>Logo<a href='https://py.pl/fIqHn'style={{textDecoration:'none', color:'#e4b85d'}} >48 x 48 (B)</a></th>
-                      <th>Logo <a href='' style={{textDecoration:'none', color:'#e4b85d'}}>Download File</a></th>
+                      <th><a href='' style={{textDecoration:'none', color:'#e4b85d'}}>192 x 192 (W)</a></th>
+                      <th><a href=''style={{textDecoration:'none', color:'#e4b85d'}} >24 x 24 (B)</a></th>
+                      <th> <a href='' style={{textDecoration:'none', color:'#e4b85d'}}>1000 x 1000 (B)</a></th>
                     </tr>
                   </tbody>
               </Table>
